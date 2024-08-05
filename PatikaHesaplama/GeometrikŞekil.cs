0e6d4fcd06e112a9d5582a41f7748bea86ccea0e
Emrullah Cevizbaş <cevizbas01@gmail.com>
2024-08-05 19:57:52 +0300

    Proje dosyası ekle.

---
+++ PatikaHesaplama/GeometrikŞekil.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PatikaHesaplama
+{
+    public class GeometrikŞekil//En genel class
+    {
+        int _yükseklik;
+        int _kenarUzunluğu;
+        public int Yukseklik {  get; set; }
+        public int KenarUuzunluğu {  get; set; }
+        public virtual void VeriGirme(int Yukseklik , int KenarUzunluğu)// Genel metot verileri topluyoruz
+        {
+            Console.WriteLine("Yükseklik ve kenar uzunluğu girildi");
+        }
+        
+    }   
+    public class Kare:GeometrikŞekil
+    {
+        public override void VeriGirme(int Yukseklik, int KenarUzunluğu)// metotu çağırarak özelleştiriyoruz
+        {
+            base.VeriGirme(Yukseklik, KenarUzunluğu);
+            double Alan = Yukseklik * KenarUzunluğu;
+            Console.WriteLine("Karenin alanı " + Alan);// kare alanı hesaplamak için işelmeler
+        }
+
+    }
+    public class Dikdörtgen : GeometrikŞekil
+    {
+        public override void VeriGirme(int Yukseklik, int KenarUzunluğu)
+        {
+            base.VeriGirme(Yukseklik, KenarUzunluğu);
+            double Alan = Yukseklik * KenarUzunluğu;//dikdörtgenin alanı hesaplama 
+            Console.WriteLine("Dikdörtgen alanı" +Alan);
+
+        }
+    }
+    public class Üçgen : GeometrikŞekil
+    {
+        public override void VeriGirme(int Yukseklik, int KenarUzunluğu)
+        {
+            base.VeriGirme(Yukseklik, KenarUzunluğu);
+            double Çarpım = Yukseklik * KenarUzunluğu;
+            double Alan = Çarpım / 2;//bölme işlemi olduğu için double kullandım
+            Console.WriteLine("Üçgenin alanı" +Alan);
+        }
+    }
+}
---
+++ PatikaHesaplama/Program.cs
@@ -0,0 +1,10 @@
+﻿using PatikaHesaplama;
+
+GeometrikŞekil Kare = new Kare();
+Kare.VeriGirme(8, 40);
+
+GeometrikŞekil dikdörtgen = new Dikdörtgen();
+dikdörtgen.VeriGirme(44, 10);
+
+GeometrikŞekil üçgen =new Üçgen();
+üçgen.VeriGirme(60,5);
